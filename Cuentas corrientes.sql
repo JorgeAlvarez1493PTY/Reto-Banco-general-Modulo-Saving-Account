8efbc5d89d1b0103c8c920a324363f57a7a64c36
JorgeAlvarez1493PTY <77408044+JorgeAlvarez1493PTY@users.noreply.github.com>
2021-01-16 21:18:50 -0500

    Delete Cuentas corrientes.sql

--- Cuentas corrientes.sql
+++
@@ -1,9 +0,0 @@
-DELIMITER //
-CREATE PROCEDURE getcurrentaccounts()
-BEGIN
-   select Account_Name, Account_number, Balance from taccounts1  where currentac ='Y' ;
-   END //
-
-DELIMITER ;
-
-COMMIT;
